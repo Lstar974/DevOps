31d68ad513935161d2a70f48372cad959273bf2a
Lstar974 <112543639+Lstar974@users.noreply.github.com>
2022-12-24 16:27:43 +0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public  class Main {  
+  
+     private  static  int i ;  
+      
+     public  static  void main(String [] args) {  
+        System.out.println("i + 1 = " + (i +  1)) ;  
+          
+    }  
+}
